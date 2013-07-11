e2a317d490365a5ee4503a12b46f9daea4279645
Donald <donald@thehoefer.com>
2013-07-11 09:56:22 -0400

    creation of custom view

---
+++ DragAndDraw/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by panda on 7/11/13.
+ */
+public class BoxDrawingView extends View {
+    // Used when creating the view in code
+    public BoxDrawingView(Context context) {
+        this(context, null);
+    }
+
+    // Used when inflating the view from XML
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
