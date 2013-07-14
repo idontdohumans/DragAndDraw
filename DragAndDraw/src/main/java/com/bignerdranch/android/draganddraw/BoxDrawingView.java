e885921a43791cd95a36912927f5030c31b5bcbd
Donald <donald@thehoefer.com>
2013-07-13 20:06:56 -0400

    Logging screen touch/drag events.

--- DragAndDraw/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
+++ DragAndDraw/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -1,13 +1,18 @@
 package com.bignerdranch.android.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
  * Created by panda on 7/11/13.
  */
 public class BoxDrawingView extends View {
+    public static final String TAG = "BoxDrawingView";
+
     // Used when creating the view in code
     public BoxDrawingView(Context context) {
         this(context, null);
@@ -16,5 +21,27 @@ public class BoxDrawingView extends View {
     // Used when inflating the view from XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF curr = new PointF(event.getX(), event.getY());
+
+        Log.i(TAG, "Received event at x=" + curr.x + ", y=" + curr.y + ":");
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.i(TAG, " ACTION_DOWN");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, " ACTION_MOVE");
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.i(TAG, " ACTION_UP");
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                Log.i(TAG, " ACTION_CANCEL");
+                break;
+        }
+
+        return true;
     }
 }
